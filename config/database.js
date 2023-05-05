91ac11de181567e30657551c8914a161ff0eabb4
Azam Suiev <asufiev@rightandabove.com>
2023-05-06 00:52:14 +0200

    added setong

--- config/database.js
+++ config/database.js
@@ -1,35 +1,36 @@
 // if mongodb 
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'mongoose',
-      settings: {
-        uri: env('DATABASE_URI'),
-      },
-      options: {
-        ssl: true,
-      },
-    },
-  },
-});
-
-//ifpostgres Database
 // module.exports = ({ env }) => ({
 //   defaultConnection: 'default',
 //   connections: {
 //     default: {
-//       connector: 'bookshelf',
+//       connector: 'mongoose',
 //       settings: {
-//         client: 'postgres',
-//         host: env('DATABASE_HOST', 'localhost'),
-//         port: env.int('DATABASE_PORT', 5432),
-//         database: env('DATABASE_NAME', 'strapi3'),
-//         username: env('DATABASE_USERNAME', undefined),
-//         password: env('DATABASE_PASSWORD', undefined),
-//         ssl: env.bool('DATABASE_SSL', false),
+//         uri: env('DATABASE_URI'),
 //       },
-//       options: {}
+//       options: {
+//         ssl: true,
+//       },
 //     },
 //   },
 // });
+
+//ifpostgres Database
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi3'),
+        username: env('DATABASE_USERNAME', undefined),
+        password: env('DATABASE_PASSWORD', undefined),
+        ssl: env.bool('DATABASE_SSL', false),
+        url: env('DATABASE_URL'),
+      },
+      options: {}
+    },
+  },
+});
