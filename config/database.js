d9eb66bb02dffd707c178b5db246ccb7e2566b91
Azam Suiev <asufiev@rightandabove.com>
2023-05-06 02:40:10 +0200

    setting

--- config/database.js
+++ config/database.js
@@ -22,16 +22,15 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', undefined),
-        password: env('DATABASE_PASSWORD', undefined),
-        ssl: env.bool('DATABASE_SSL', false),
         url: env('DATABASE_URL'),
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
-      options: {}
+      options: {},
     },
   },
 });
+
+
 
